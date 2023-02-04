199e650f851ff55b0a012cb088c8c32d6059a11a
mohmmad <azimi.jozani.mohammad@gmail.com>
2023-02-04 18:42:50 +0330

    write test for collection again

--- app/store/tests/test_collections.py
+++ app/store/tests/test_collections.py
@@ -1,9 +1,10 @@
 from rest_framework import status
+from django.contrib.auth.models import User
 from rest_framework.test import APIClient
 import pytest
 
 
-
+@pytest.mark.django_db
 class TestCreateCollection:
     def test_if_user_is_anonymous_return_401(self):
         client = APIClient()
@@ -12,3 +13,23 @@ class TestCreateCollection:
         }
         response = client.post('/store/collections/', {"title": "test"})
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_return_403(self):
+        client = APIClient()
+        client.force_authenticate(user={})
+        payload = {
+            'title': "a"
+        }
+        response = client.post('/store/collections/', {"title": "test"})
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_data_is_valid_return_200(self):
+        client = APIClient()
+        client.force_authenticate(User(is_staff=True))
+        payload = {
+            'title': "a"
+        }
+        response = client.post('/store/collections/', {"title": "test"})
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data['id'] > 0
+
